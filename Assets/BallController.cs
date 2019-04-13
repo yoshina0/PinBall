7656329352de518ac5fed758b4a34b261166904b
yosinao yabe <freesia.4470@gmail.com>
2019-04-13 23:40:52 +0900

    PinBallプロジェクト　課題初回コミット
    カリキュラムLesson5の課題・発展課題を含むファイルです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject ScoreText;
+
+    //スコア用の変数（初期値0）
+    private int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            //GameOverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+        //ScoreTextにスコア初期値を表示
+        this.ScoreText.GetComponent<Text>().text = "SCORE :" + score;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //タグの情報を取得しcollisionに代入
+        string TagName = collision.gameObject.tag;
+
+        //タグに応じて点数加算
+        if(TagName == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if(TagName == "LargeStarTag")
+        {
+            this.score += 100;
+        }
+        else if(TagName == "SmallCloudTag")
+        {
+            this.score += 50;
+        }
+        else if (TagName == "LargeCloudTag")
+        {
+            this.score += 150;
+        }
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印キーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") { SetAngle(this.flickAngle); }
+
+        //右矢印キーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") { SetAngle(this.flickAngle); }
+
+        //矢印キーが離された時フリッパーをもとに戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") { SetAngle(this.defaultAngle); }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") { SetAngle(this.defaultAngle); }
+
+
+        //ここから発展課題
+        Touch touch = Input.GetTouch(0);
+
+        //マルチタッチを扱えるように配列の変数を用意
+        Touch[] touches = Input.touches;
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            //タッチした時
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (touches[i].position.x < Screen.width / 2 && tag == "LeftFripperTag") { SetAngle(this.flickAngle); }
+
+                if (touches[i].position.x >= Screen.width / 2 && tag == "RightFripperTag") { SetAngle(this.flickAngle); }
+            }
+
+            //タッチした指が離された時
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (touches[i].position.x < Screen.width / 2 && tag == "LeftFripperTag") { SetAngle(this.defaultAngle); }
+
+                if (touches[i].position.x >= Screen.width / 2 && tag == "RightFripperTag") { SetAngle(this.defaultAngle); }
+            }
+        }
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle (float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
